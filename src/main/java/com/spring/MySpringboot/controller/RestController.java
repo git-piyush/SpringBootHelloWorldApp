8c5b1e60b86549d32d7a28b7920b99055e816781
Piyush Kumar <piyush-kumar.piyush@dxc.com>
2020-04-08 13:16:45 +0530

    First commit

---
+++ src/main/java/com/spring/MySpringboot/controller/RestController.java
@@ -0,0 +1,18 @@
+package com.spring.MySpringboot.controller;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.web.bind.annotation.RestController
+@RequestMapping("/")
+public class RestController {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello World!! The current Date and time is "+LocalDateTime.now();
+	}
+}
